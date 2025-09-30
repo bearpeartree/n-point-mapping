6386927f3a59d18108e469597503c4a2867a0747
Lisa <tranlisa@live.de>
2025-09-30 13:01:26 +0200

    Completed check_recomb

--- main.py
+++ main.py
@@ -30,10 +30,13 @@ def helper_id(gene1, gene2, gene3, current_gamete):
 
 def check_recomb(gamete, first_gene, second_gene):
     splitted_genes = gamete.split("/")
+    cp_splitted_genes = splitted_genes.copy()
     recomb = []
-    for i in splitted_genes:
-        if i[0] == first_gene or i[0] == second_gene:
-            recomb.append(i)
+    for i in range(0, len(cp_splitted_genes)):
+        if '+' in cp_splitted_genes[i]:
+            cp_splitted_genes[i] = cp_splitted_genes[i].replace('+','')
+        if cp_splitted_genes[i] == first_gene or cp_splitted_genes[i] == second_gene:
+            recomb.append(splitted_genes[i])
     
     if ('+' in recomb[0] and '+' in recomb[1]) or '+' not in recomb[0] and '+' not in recomb[1]:
         return "parental"
--- tests.py
+++ tests.py
@@ -11,3 +11,13 @@ def test_check_parental_2_combo():
     test_gamete = "A/B/C+"
 
     assert check_recomb(test_gamete, 'A', 'B') == "parental"
+
+
+def test_check_recombi_combo():
+    test_gamete = "vg/cv+/e+"
+
+    assert check_recomb(test_gamete, 'cv', 'vg') == "recombinental"
+
+
+def test_check_recombi_2_combo():
+    assert check_recomb("A/B/C+", 'A', 'C') == "recombinental"
