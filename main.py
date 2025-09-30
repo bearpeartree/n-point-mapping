d1dc4a266929d6c350a2504e59d11514788a2820
Lisa <tranlisa@live.de>
2025-09-30 12:37:46 +0200

    Add check_recomb

--- main.py
+++ main.py
@@ -2,14 +2,44 @@ def main():
     print(process_raw_gametes("Examples/gametes_example_1.txt"))
 
 
-# (gamete, count)
 def process_raw_gametes(filename):
-    gametes = []
+    gametes = {}
     with open(filename) as file:
         for  line in file:
             current = line.rstrip("\n").split(" ")
-            gametes.append((current[0], int(current[1])))
+            gametes[current[0]] = int(current[1])
     return gametes
+
+
+# TODO
+def identify_parent_or_combo(gametes):
+    pass
+
+
+# TODO
+def helper_id(gene1, gene2, gene3, current_gamete):
+    genes12 = []
+    genes13 = []
+    genes23 = []
+
+    check_recomb(current_gamete, gene1, gene2)
+    check_recomb(current_gamete, gene1, gene3)
+    check_recomb(current_gamete, gene2, gene3)
+
+
+
+def check_recomb(gamete, first_gene, second_gene):
+    splitted_genes = gamete.split("/")
+    recomb = []
+    for i in splitted_genes:
+        if i[0] == first_gene or i[0] == second_gene:
+            recomb.append(i)
+    
+    if ('+' in recomb[0] and '+' in recomb[1]) or '+' not in recomb[0] and '+' not in recomb[1]:
+        return "parental"
+    else:
+        return "recombinental"
+
 
 if __name__ == "__main__":
     main()
